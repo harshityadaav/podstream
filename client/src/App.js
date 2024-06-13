95bfc96d2ba6923ef159d39e661a50b9ed7506d2
harshityadaav <harshit2622002@gmail.com>
2024-06-13 14:59:24 +0530

    dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './utils/Themes';
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+`;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div >
-      PODSTREAM
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>Podstream</Container>
+    </ThemeProvider>
   );
 }
 
