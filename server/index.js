0d0160ec2b19caaeb915bf22b97a3f69215b7507
harshityadaav <harshit2622002@gmail.com>
2024-07-06 01:00:33 +0530

    st up backend

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 8700;
+
+const connect = () => {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log('MongoDB connected');
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+
+
+app.use(express.json())
+
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    })
+})
+
+app.listen(port, () => {
+    console.log("Connected")
+    connect();
+})
